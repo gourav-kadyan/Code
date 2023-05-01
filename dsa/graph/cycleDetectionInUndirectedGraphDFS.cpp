96089c60920352a9cc70693953bd43c8bd39cf5f
Gourav Kadyan <69764718+gourav-kadyan@users.noreply.github.com>
2023-05-02 03:09:42 +0530

    did cycleDetectionInUndirectedGraphBFS and DFS also

--- dsa/graph/cycleDetectionInUndirectedGraphDFS.cpp
+++ dsa/graph/cycleDetectionInUndirectedGraphDFS.cpp
@@ -8,28 +8,19 @@
 
 using namespace std;
 
-bool bfs(int src, unordered_map<int,list<int> > &adj, unordered_map<int, bool> &visited){
-    unordered_map<int,int> parent;
-    parent[src] = -1;
-    visited[src] = true;
-    queue<int> q;
-    q.push(src);
-
-    while(!q.empty()){
-        int front = q.front();
-        q.pop();
-        for(auto neighbour : adj[front]){
-            if(visited[neighbour] == true && neighbour != parent[front]){
+bool dfs(int node,int parent,unordered_map<int,list<int>> &adj,unordered_map<int,bool> &visited){
+    visited[node] = true;
+    for(auto neighbour:adj[node]){
+        if(!visited[neighbour]){
+            bool cycle = dfs(neighbour,node,adj,visited);
+            if(cycle == true){
                 return true;
             }
-            else if(!visited[neighbour]){
-                q.push(neighbour);
-                visited[neighbour] = true;
-                parent[neighbour] = front;
-            }
+        }
+        else if(neighbour != parent){
+            return true;
         }
     }
-
     return false;
 }
 
@@ -47,7 +38,7 @@ bool find_cycle_in_undirected_graph(vector<vector<int>> &edges,int n, int m){
     unordered_map<int,bool> visited;
     for(int i=0;i<n;i++){
         if(!visited[i]){
-            bool ans = bfs(i,adj,visited);
+            bool ans = dfs(i,i,adj,visited);
             if(ans == true){
                 return true;
             }
